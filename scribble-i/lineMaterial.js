9472089d1ba5dd4a9e658363122f5ee2fc8b8848
thespite <the.spite@gmail.com>
2020-10-11 17:01:21 +0100

    added scribble i

---
+++ scribble-i/lineMaterial.js
@@ -0,0 +1,154 @@
+import {
+  MeshStandardMaterial,
+  RepeatWrapping,
+  TextureLoader,
+  Vector2,
+  Vector3,
+} from "../third_party/three.module.js";
+import { lines } from "../shaders/lines.js";
+
+class LineMaterial extends MeshStandardMaterial {
+  constructor(options) {
+    super(options);
+    const self = this;
+
+    this.onBeforeCompile = (shader, renderer) => {
+      const loader = new TextureLoader();
+      const texture = loader.load("../assets/Craft_Rough.jpg");
+      const noiseTexture = loader.load("../assets/noise1.png");
+      noiseTexture.wrapS = noiseTexture.wrapT = RepeatWrapping;
+      shader.uniforms.resolution = { value: new Vector2(1, 1) };
+      shader.uniforms.paperTexture = { value: texture };
+      shader.uniforms.noiseTexture = { value: noiseTexture };
+      shader.uniforms.range = { value: new Vector2(0.25, 0.75) };
+      shader.uniforms.range2 = { value: new Vector2(0.5, 0.5) };
+      shader.uniforms.scale = { value: 1 };
+      shader.uniforms.radius = { value: 1 };
+      self.uniforms = shader.uniforms;
+      shader.vertexShader = shader.vertexShader.replace(
+        `#include <common>`,
+        `#include <common>
+        out vec2 vCoords;
+        out vec3 vPosition;
+        out vec4 vWorldPosition;`
+      );
+      shader.vertexShader = shader.vertexShader.replace(
+        `#include <uv_vertex>`,
+        `#include <uv_vertex>
+        vCoords = uv;
+        vPosition = position;
+        vWorldPosition = modelViewMatrix * vec4(position, 1.);`
+      );
+
+      shader.fragmentShader = shader.fragmentShader.replace(
+        `#include <common>`,
+        `#include <common>
+        uniform vec2 resolution;
+        uniform sampler2D paperTexture;
+        uniform sampler2D noiseTexture;
+        uniform vec2 range;
+        uniform vec2 range2;
+        uniform float scale;
+        uniform float radius;
+        in vec2 vCoords;
+        in vec3 vPosition;
+        in vec4 vWorldPosition;
+        #define TAU 6.28318530718
+
+        mat2 m = mat2( vec2 (2.1, 1.0), vec2(-1.0, 2.1) );
+
+        float noise_f(vec2 p) {
+          return sin(1.66*p.x)*sin(1.66*p.y);
+        }
+
+        float warp(in vec2 p) {
+          float f = 0.0;
+          float amp = 1.0; 
+          float kAmp = 0.5;
+          float freq = 0.63;
+          float kFreq = 1.53;
+          
+          f += amp*noise_f(p); p = m*p*freq; amp *=kAmp; freq *=kFreq;
+          f += amp*noise_f(p); p = m*p*freq; amp *=kAmp; freq *=kFreq;
+          f += amp*noise_f(p); p = m*p*freq; amp *=kAmp; freq *=kFreq;
+          f += amp*noise_f(p);
+          
+          f = fract(f);
+          
+          return f;
+        }
+   
+        float aastep(float threshold, float value) {
+          #ifdef GL_OES_standard_derivatives
+            float afwidth = length(vec2(dFdx(value), dFdy(value))) * 0.70710678118654757;
+            return smoothstep(threshold-afwidth, threshold+afwidth, value);
+          #else
+            return step(threshold, value);
+          #endif  
+        }
+
+        #define tileSize 0.05
+        #define tileCenter vec2(tileSize / 2.0, tileSize / 2.0)
+
+        float scribble(in vec2 uv, in float q) {
+          
+          vec2 tile = vec2(floor(uv.x / tileSize), floor(uv.y / tileSize));
+          uv -= tile * tileSize;
+          float dist = length(tileCenter - uv) + warp(uv);
+          float circle = .5 + .5 * cos(dist * 1000. + 100. * warp(uv.yx));      
+          return aastep(.4, circle+q);
+        }
+
+        /////////
+
+        float texcube(in vec3 p, in vec3 n, in float q) {
+          //p = .05 * vec3(warp(p.xy), warp(p.xy), warp(p.xy));
+          vec3 v = vec3(scribble(p.yz,q), scribble(p.zx,q), scribble(p.xy,q));
+          //p = 1. * vec3(warp(p.xy), warp(p.xy), warp(p.xy));
+          p *= 1.1;
+          vec3 v2 = vec3(scribble(p.yz,q), scribble(p.zx,q), scribble(p.xy,q));
+          v *= v2;
+          return dot(v, n*n);
+        }
+
+        float luma(vec3 color) {
+          return dot(color, vec3(0.299, 0.587, 0.114));
+        }
+        float luma(vec4 color) {
+          return dot(color.rgb, vec3(0.299, 0.587, 0.114));
+        }
+        float blendColorBurn(float base, float blend) {
+          return (blend==0.0)?blend:max((1.0-((1.0-base)/blend)),0.0);
+        }
+        
+        vec3 blendColorBurn(vec3 base, vec3 blend) {
+          return vec3(blendColorBurn(base.r,blend.r),blendColorBurn(base.g,blend.g),blendColorBurn(base.b,blend.b));
+        }
+        
+        vec3 blendColorBurn(vec3 base, vec3 blend, float opacity) {
+          return (blendColorBurn(base, blend) * opacity + base * (1.0 - opacity));
+        }
+        `
+      );
+      shader.fragmentShader = shader.fragmentShader.replace(
+        "#include <dithering_fragment>",
+        `#include <dithering_fragment>
+        float l = luma(gl_FragColor.rgb);
+        ivec2 size = textureSize(paperTexture, 0);
+        vec4 paper = texture(paperTexture, gl_FragCoord.xy / vec2(float(size.x), float(size.y)));
+
+        vec3 coords = .1 * vWorldPosition.xyz / vWorldPosition.w;
+        float line = texcube(coords, vNormal, l);
+
+        vec3 inkColor = vec3(24., 49., 211.)/255.;
+        float r = aastep(1.-l, line);
+
+        gl_FragColor.rgb = blendColorBurn(paper.rgb, inkColor, 1.-r);
+        //gl_FragColor.rgb = vec3(r);
+        `
+      );
+    };
+  }
+}
+
+export { LineMaterial };
---
+++ scribble-i/main.js
@@ -0,0 +1,174 @@
+import {
+  WebGLRenderer,
+  PerspectiveCamera,
+  Scene,
+  Mesh,
+  DirectionalLight,
+  HemisphereLight,
+  Vector3,
+  Raycaster,
+  PCFSoftShadowMap,
+  sRGBEncoding,
+  Vector2,
+  IcosahedronBufferGeometry,
+  TorusKnotBufferGeometry,
+  PointLight,
+  DoubleSide,
+  CubeTextureLoader,
+  AmbientLight,
+  MeshStandardMaterial,
+} from "../third_party/three.module.js";
+import { OrbitControls } from "../third_party/OrbitControls.js";
+import { LineMaterial } from "./lineMaterial.js";
+import * as dat from "../third_party/dat.gui.module.js";
+import { init as initBlob } from "../js/sceneBlob.js";
+import { init as initTorus } from "../js/sceneTorus.js";
+import { init as initBackdrop } from "../js/sceneBackdrop.js";
+import { init as initSpheres } from "../js/sceneSpheres.js";
+import { init as initSuzanne } from "../js/sceneSuzanne.js";
+
+const params = {
+  roughness: 0.2,
+  metalness: 0.1,
+  min: 0.25,
+  max: 0.75,
+  min2: 0.5,
+  max2: 0.5,
+  scale: 1,
+  radius: 3,
+};
+const gui = new dat.GUI();
+gui.add(params, "roughness", 0, 1);
+gui.add(params, "metalness", 0, 1);
+
+gui.add(params, "min", 0, 2);
+gui.add(params, "max", 0, 2);
+gui.add(params, "min2", 0, 2);
+gui.add(params, "max2", 0, 2);
+gui.add(params, "scale", 0, 10);
+gui.add(params, "radius", 1, 10);
+const canvas = document.querySelector("canvas");
+
+const renderer = new WebGLRenderer({
+  antialias: true,
+  alpha: true,
+  canvas,
+  preserveDrawingBuffer: false,
+  powerPreference: "high-performance",
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0xffffff, 1);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
+//renderer.outputEncoding = sRGBEncoding;
+//renderer.gammaFactor = 2.2;
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(60, 1, 0.1, 100);
+camera.position.set(0, 0, 10);
+
+const loader = new CubeTextureLoader();
+loader.setPath("../assets/");
+
+const textureCube = loader.load([
+  "posx.jpg",
+  "negx.jpg",
+  "posy.jpg",
+  "negy.jpg",
+  "posz.jpg",
+  "negz.jpg",
+]);
+textureCube.encoding = sRGBEncoding;
+
+const material = new LineMaterial({
+  color: 0x808080,
+  roughness: 0.2,
+  metalness: 0.1,
+  envMap: textureCube,
+  side: DoubleSide,
+});
+
+initBackdrop(scene, material);
+initSuzanne(scene, material);
+//initTorus(scene, material);
+//initBlob(scene, material);
+//initSpheres(scene, material);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.screenSpacePanning = true;
+
+const raycaster = new Raycaster();
+const mouse = new Vector2();
+
+function onMouseMove(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+window.addEventListener("mousemove", onMouseMove, false);
+
+const up = new Vector3(0, 1, 0);
+
+function resize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const dPR = window.devicePixelRatio;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+}
+
+const tmp = new Vector2();
+function render() {
+  if (material.uniforms && material.uniforms.resolution) {
+    renderer.getSize(tmp);
+    tmp.multiplyScalar(window.devicePixelRatio);
+    material.uniforms.resolution.value.copy(tmp);
+
+    material.roughness = params.roughness;
+    material.metalness = params.metalness;
+
+    material.uniforms.range.value.set(params.min, params.max);
+    material.uniforms.range2.value.set(params.min2, params.max2);
+    material.uniforms.scale.value = params.scale;
+    material.uniforms.radius.value = params.radius;
+  }
+  renderer.render(scene, camera);
+  renderer.setAnimationLoop(render);
+}
+
+const light = new DirectionalLight(0xffffff, 0.5);
+light.position.set(0, 1, 3);
+light.castShadow = true;
+light.shadow.bias = -0.0001;
+light.shadow.mapSize.set(4096, 4096);
+scene.add(light);
+
+const light2 = new DirectionalLight(0xffffff, 0.5);
+light2.position.set(-3, -3, -3);
+light2.castShadow = true;
+light2.shadow.bias = -0.0001;
+light2.shadow.mapSize.set(4096, 4096);
+scene.add(light2);
+
+const hemiLight = new HemisphereLight(0xbbbbbb, 0x080808, 1);
+scene.add(hemiLight);
+
+const ambientLight = new AmbientLight(0x202020);
+//scene.add(ambientLight);
+
+const spotLight = new PointLight(0xa183ff, 1);
+spotLight.castShadow = true;
+spotLight.distance = 8;
+spotLight.decay = 2;
+spotLight.power = 40;
+//scene.add(spotLight);
+
+async function init() {
+  resize();
+  render();
+}
+
+window.addEventListener("resize", resize);
+
+init();
