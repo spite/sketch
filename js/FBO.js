e8f03bf1f27e3581c235e7005ee0f42f8024c4c7
Jaume Sanchez <the.spite@gmail.com>
2026-08-01 22:50:25 +0100

    Update FBO.js

--- js/FBO.js
+++ js/FBO.js
@@ -8,14 +8,14 @@ import {
 
 function getFBO(w, h, options = {}) {
   const fbo = new WebGLRenderTarget(w, h, {
-    wrapS: options.wrapS || ClampToEdgeWrapping,
-    wrapT: options.wrapT || ClampToEdgeWrapping,
-    minFilter: options.minFilter || LinearFilter,
-    magFilter: options.magFilter || LinearFilter,
-    format: options.format || RGBAFormat,
-    type: options.type || UnsignedByteType,
-    stencilBuffer: options.stencilBuffer || false,
-    depthBuffer: options.depthBuffer || true,
+    wrapS: options.wrapS ?? ClampToEdgeWrapping,
+    wrapT: options.wrapT ?? ClampToEdgeWrapping,
+    minFilter: options.minFilter ?? LinearFilter,
+    magFilter: options.magFilter ?? LinearFilter,
+    format: options.format ?? RGBAFormat,
+    type: options.type ?? UnsignedByteType,
+    stencilBuffer: options.stencilBuffer ?? false,
+    depthBuffer: options.depthBuffer ?? true,
   });
   return fbo;
 }
